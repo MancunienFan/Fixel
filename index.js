4afde3a65f849cdd167ea218fdd712c22db1c921
ZAKARIA DJELLAL <zakaria.djellal@racj.gouv.qc.ca>
2025-06-20 16:56:20 -0400

    connexion a mango fonctionnelle

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const { MongoClient } = require('mongodb');
+//mongodb+srv://fixel:<db_password>@cluster0.hfa6e5c.mongodb.net/const { MongoClient } = require('mongodb');
 
 // Chaîne de connexion à remplacer avec ton mot de passe et ta base
-const uri = "mongodb://fixel:chicharito@cluster0-shard-00-00.xxxx.mongodb.net:27017,cluster0-shard-00-01.xxxx.mongodb.net:27017,cluster0-shard-00-02.xxxx.mongodb.net:27017/sample_mflix?ssl=true&replicaSet=atlas-xxxx&authSource=admin&retryWrites=true&w=majority"
-
+//const uri = "mongodb://fixel:chicharito@cluster0-shard-00-00.xxxx.mongodb.net:27017,cluster0-shard-00-01.xxxx.mongodb.net:27017,cluster0-shard-00-02.xxxx.mongodb.net:27017/sample_mflix?ssl=true&replicaSet=atlas-xxxx&authSource=admin&retryWrites=true&w=majority"
+const uri = "mongodb+srv://fixel:chicharito@cluster0.hfa6e5c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const client = new MongoClient(uri);
 
 async function run() {
@@ -14,9 +15,9 @@ async function run() {
     const collection = db.collection("users");
 
     // Exemple : Insérer un document
-    const nouveauProduit = { name: "Test produit", password: 100, enStock: true };
+    /*const nouveauProduit = { name: "Test produit", password: 100, enStock: true };
     const resultat = await collection.insertOne(nouveauProduit);
-    console.log("Document inséré avec l'ID :", resultat.insertedId);
+    console.log("Document inséré avec l'ID :", resultat.insertedId);*/
 
     // Exemple : Lire les documents
     const produits = await collection.find({}).toArray();
