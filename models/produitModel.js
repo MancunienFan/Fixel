a9ebb2e51d87f8338fbec1938695b9979603f6e1
ZAKARIA DJELLAL <zakaria.djellal@racj.gouv.qc.ca>
2025-06-28 16:22:12 -0400

    maj index tableau

--- models/produitModel.js
+++ models/produitModel.js
@@ -2,9 +2,28 @@ const mongoose = require('mongoose');
 
 const produitSchema = new mongoose.Schema({
   nom: String,
-  etat: String,
+ statut: {
+    type: String,
+    enum: ["neuf", "bon état", "mauvais état"],
+    required: true,
+    default: "neuf",
+  },
   prix: Number,
-  categorie: String
+  categorie: String,
+  etatbatterie: Number, 
+  model: String,
+  prixachat: Number,
+  prixvente: Number,
+   dateachat: {
+    type: Date,
+    default: Date.now  // 👈 par défaut, date actuelle
+  },
+ datevente: {
+  type: Date,
+  default: null
+}
+
+
 });
 
 module.exports = mongoose.model('Produit', produitSchema);
