3da4555a7c3e4bf152b4f3d8a84b769f16b22878
ZAKARIA DJELLAL <zakaria.djellal@racj.gouv.qc.ca>
2025-07-02 13:25:38 -0400

    fix bug utilisateur routes 2

--- routes/utilisateurRoutes.js
+++ routes/utilisateurRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Utilisateur = require('../models/Utilisateur');
+const Utilisateur = require('../models/utilisateur');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
